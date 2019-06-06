095863b53cc964727d5c85b4367e62428035c7a9
janelle-nomad <janelle.saige@gmail.com>
2019-06-06 12:07:01 -0400

    removed test files, to confirm react is working, added Tachyons

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
+// Custom imports
+import 'tachyons';
 
-ReactDOM.render(<h1>Robo Buddies</h1>, document.getElementById('root'));
+
+ReactDOM.render(<Card/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
